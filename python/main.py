73b577354394a0514d285f17646ab9253da969ab
EvanRJenkins <JenkinsE@jbu.edu>
2026-03-07 12:56:36 -0600

    Added graph widget, reorganizing with structured HBox layout.

--- python/main.py
+++ python/main.py
@@ -3,9 +3,10 @@ import serial
 import csv
 import time
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, 
-                             QPlainTextEdit, QPushButton)
+                             QPlainTextEdit, QPushButton, )
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
 from SubmoduleThreads import TelemetryReceiverThread
+import pyqtgraph as pg # This is for all live plots
 
 # Main GUI
 class GUIThread(QMainWindow):
@@ -17,13 +18,26 @@ class GUIThread(QMainWindow):
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
 
-        # Message Box
+        # Message box (logging terminal)
         self.message_box = QPlainTextEdit()
         self.message_box.setReadOnly(True)
         self.message_box.setStyleSheet("background-color: #121212; color: #00FF41; font-family: 'Consolas'; font-size: 13pt;")
         self.layout.addWidget(self.message_box)
 
-        # Start Button
+        # Live line graph 1
+        self.line_graph1 = pg.PlotWidget()
+        self.line_graph1.setBackground('#1a1a1a')
+        self.line_graph1.setTitle("Live Telemetry Temp", color="w", size="15pt")
+        self.line_graph1.showGrid(x=True, y=True)
+        
+        # Create a line reference
+        self.data_x = list(range(100))  # 100 time points
+        self.data_y = [0] * 100         # Start with 100 zeros
+        self.line = self.line_graph1.plot(self.data_x, self.data_y, pen=pg.mkPen(color='#00FF41', width=2))
+        
+        self.layout.addWidget(self.line_graph1, 2) # Weight of 2 (Graph is wider)
+
+        # Start button
         self.start_btn = QPushButton("Start Telemetry Receiver")
         self.start_btn.setFixedHeight(50)
         self.start_btn.clicked.connect(self.start_telemetry)
@@ -45,10 +59,18 @@ class GUIThread(QMainWindow):
             self.message_box.verticalScrollBar().maximum()
         )
 
+    def update_graph(self, new_value):
+        """
+        Call every time a new TLM packet arrives (or when we want to plot one).
+        """
+        self.data_y = self.data_y[1:]  # Remove oldest data point
+        self.data_y.append(float(new_value))  # Add new telemetry point
+        self.line.setData(self.data_x, self.data_y)
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_Escape:
             self.telemetry_thread.requestInterruption()
-            self.telemetry_thread.wait() # Cleanly close thread
+            self.telemetry_thread.wait() # Close thread
             self.close()
 
 if __name__ == "__main__":
